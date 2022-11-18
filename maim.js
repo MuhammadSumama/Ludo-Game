c5cb6548c3bce318b5701b255b016619c72acac6
MuhammadSumama <muhammadsumamajujhara78@gmail.com>
2022-11-18 14:13:05 +0500

    done

--- maim.js
+++ maim.js
@@ -21,35 +21,4 @@ if (random1 > random2) {
 }else{
     document.querySelector("h1").textContent= "Match Draw";
 }
-/////////========
-// /Player 1 
-// var random1 = Math.floor(Math.random()*6)+1;
-// var dice1= document.querySelector('.img1');
-// var select1 = "/images" + "/dice" + random1 + ".png";
-
-// dice1.setAttribute("src",select1);
-// var random2 = Math.floor(Math.random()*6)+1;
-
-// var dice2= document.querySelector('.img2');
-
-
-// var select2 = "/images" + "/dice" + random2 + ".png";
-
-
-// dice2.setAttribute("src",select2);
-
-
-
-// if(random1>random2){
-//     document.querySelector('h1').textContent = "Player 1 Won The Match";
-
-// }
-
-// else if(random1 < random2)
-// {
-//     document.querySelector('h1').textContent = "Player 2 Won The Match"
-// }
-
-// else{
-//     document.querySelector('h1').textContent = "Match Draw";
-// }
+\
